a7fe011bed6ea3a32039c4319f91b6fda7c42d46
martynovappolina <79730482+martynovappolina@users.noreply.github.com>
2025-02-12 12:27:56 +0300

    add font variables

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Hello World</div>
+  <div id="sputnik-ui">
+    <h1>Hello world!</h1>
+    <h2>Hello world!</h2>
+    <p>Hello world!</p>
+  </div>
 </template>
 
-<style>
-#app {
+<script>
 
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+#sputnik-ui {
+  @include text;
+  
+  h1 {
+    @include h1;
+  }
+  h2 {
+    @include h2;
+  }
 }
 </style>
