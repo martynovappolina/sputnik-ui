f6f8853efe809314ad922c6053269fe03e7c652a
martynovappolina <polly13042001@mail.ru>
2025-02-26 13:26:58 +0300

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button @click="$emit('click')">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'ButtonTemplate',
+        props: {
+            text: {
+                type: String,
+                default: 'Нажми на меня'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    button {
+        @include text;
+        border: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 16px 24px;
+        background: $primary;
+        color: $light;
+        border-radius: 7px;
+        cursor: pointer;
+        transition: all 0.2s ease;
+
+        &:hover {
+            background: $secondary;
+        }
+
+        &:active {
+            background: $secondary2;
+        }
+    }
+</style>
