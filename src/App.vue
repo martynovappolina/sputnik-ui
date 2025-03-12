f2135f2af261c215989bcf53b21826d4b521c1dc
martynovappolina <polly13042001@mail.ru>
2025-03-12 09:12:34 +0300

    add build to repository

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,60 @@
 <template>
-  <div></div>
+  <Page>
+    <Menu />
+    <Cover />
+    <HorizontalCardsBlock />
+    <VerticalCardWithImageAndText theme="dark" />
+    <NumberedList />
+    <TextBlockWithHeader theme="dark" />
+    <MultiColumnListWithIcons />
+    <Stages theme="dark" />
+    <GridWithCards />
+    <Contacts />
+    <Footer />
+  </Page>
 </template>
+
+<script>
+  import Page from './components/Page.vue';
+  import Menu from './components/Menu.vue';
+  import Cover from './components/Cover.vue';
+  import VerticalCardWithImageAndText from './components/VerticalCardWithImageAndText.vue';
+  import HorizontalCardsBlock from './components/HorizontalCardsBlock.vue';
+  import NumberedList from './components/NumberedList.vue';
+  import Contacts from './components/Contacts.vue';
+  import Footer from './components/Footer.vue';
+  import TextBlockWithHeader from './components/TextBlockWithHeader.vue';
+  import MultiColumnListWithIcons from './components/MultiColumnListWithIcons.vue';
+  import Stages from './components/Stages.vue';
+  import GridWithCards from './components/GridWithCards.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Page,
+      Menu,
+      Cover,
+      Footer,
+      VerticalCardWithImageAndText,
+      HorizontalCardsBlock,
+      Contacts,
+      NumberedList,
+      TextBlockWithHeader,
+      MultiColumnListWithIcons,
+      Stages,
+      GridWithCards
+    },
+    methods: {
+      onClickLogo() {
+        window.location.href = '/';
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+  }
+</style>
+
