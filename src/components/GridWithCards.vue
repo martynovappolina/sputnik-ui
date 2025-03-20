ab6acf8116c1d18c341c634eab7284a270e95bfa
martynovappolina <polly13042001@mail.ru>
2025-03-20 10:50:37 +0300

    Update GridWithCards.vue

--- src/components/GridWithCards.vue
+++ src/components/GridWithCards.vue
@@ -125,7 +125,7 @@
                 .image {
                     width: 100%;
                     height: 300px;
-                    background-size: cover;
+                    background-size: contain;
                     background-repeat: no-repeat;
                     background-position: center;
                 }
